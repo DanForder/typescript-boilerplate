99562c7b9400bb70d456601d5ca0e9267b90f1b5
Dan Forder <danfrdr@gmail.com>
2022-01-27 11:23:56 +0000

    files to uppercase

---
+++ src/components/Layout/Layout.spec.tsx
@@ -0,0 +1,36 @@
+import { customRender } from "../../utils/testUtils";
+import Layout from "./Layout";
+
+export default describe("Layout", () => {
+  it("Renders without children", () => {
+    const { container } = customRender(<Layout />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("Renders with children", () => {
+    const { container } = customRender(
+      <Layout>
+        <span>Child element</span>
+      </Layout>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("Renders with exampleProp enabled, no children", () => {
+    const { container } = customRender(<Layout exampleProp />);
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("Renders with exampleProp enabled, children", () => {
+    const { container } = customRender(
+      <Layout exampleProp>
+        <span>Child element</span>
+      </Layout>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+});
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,20 @@
+import "./Layout.scss";
+
+type LayoutProps = {
+  exampleProp?: boolean;
+};
+
+const ROOT_CLASSNAME = "layout";
+
+// exampleProp is a placeholder for some layout specific property that may differ from page to page; enableSidebar, showNavigation etc.
+// this can be useful for user journeys where you don't want your user to be able to navigate away as easily
+const Layout: React.FC<LayoutProps> = ({ exampleProp = false, children }) => {
+  return (
+    <div className={ROOT_CLASSNAME}>
+      {exampleProp && <span>exampleProp is true</span>}
+      <main className={`${ROOT_CLASSNAME}__main`}>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
